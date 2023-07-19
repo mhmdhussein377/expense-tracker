07f295258663b9a9f7a033770d257139ba26c340
Mohammad Hussein <mohammad.hussein377@gmail.com>
2023-07-19 16:29:28 +0300

    solved some bugs in the total amount feature

--- scripts/script.js
+++ scripts/script.js
@@ -7,7 +7,7 @@ $("form").submit(function (e) {
 function expenseItemElement(name, amount) {
     return `<div>
                     <div>${name}</div>
-                    <div>$${amount}
+                    <div class="amount">$<span class="amount-value">${amount}</span>
                         <i class="fa-solid fa-trash fa-lg remove"></i>
                     </div>
                 </div>`;
@@ -18,27 +18,47 @@ function addExpense() {
     const expenseNameInput = $("#name");
     const expenseAmountInput = $("#amount");
 
-    if (expenseAmountInput.val() === "" || expenseNameInput.val() === "") 
+    if (expenseAmountInput.val() === "" || expenseNameInput.val() === "" || isNaN(expenseAmountInput.val())) 
         return;
     
+    console.log(typeof(+ expenseAmountInput.val() === ""))
+
     const expenseItem = $(expenseItemElement(expenseNameInput.val(), expenseAmountInput.val()));
 
     expenseItem
         .find(".remove")
         .click(function () {
+            totalAmount -= + expenseItem
+                .find(".amount-value")
+                .text()
+            $(".total-value").text("$" + totalAmount);
+            if (totalAmount > 0) {
+                $(".total").show();
+            } else {
+                $(".total").hide();
+            }
             expenseItem.remove();
         });
 
     expenses.append(expenseItem);
     totalAmount += + expenseAmountInput.val();
     $(".total-value").text("$" + totalAmount);
+    if (totalAmount > 0) {
+        $(".total").show();
+    } else {
+        $(".total").hide();
+    }
     expenseNameInput.val("");
     expenseAmountInput.val("");
-}
+};
 
-$(document)
-    .ready(function () {
-        const addButton = $("button");
-        addButton.click(addExpense);
-        $(".total-value").text("$" + totalAmount);
-    });
+$(document).ready(function () {
+    const addButton = $("button");
+    addButton.click(addExpense);
+    $(".total-value").text("$" + totalAmount);
+    if (totalAmount > 0) {
+        $(".total").show()
+    } else {
+        $(".total").hide()
+    }
+});
